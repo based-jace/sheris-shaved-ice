90fe2d794d57f570f77c2625f99293b54da65c9b
Piss <Piss>
2019-03-13 10:42:29 -0500

    Authentication placeholder.

--- inv_manage/views.py
+++ inv_manage/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+def authentication(request):
+    return HttpResponse("Please login.")
+
 def home(request):
     return HttpResponse("Welcome to the inventory overview.")
 
