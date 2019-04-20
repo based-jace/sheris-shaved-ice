cd90278af6e6f2196b02a251595922ff7599218b
Piss <Piss>
2019-04-20 07:42:56 -0500

    Vue table fully sortable
    Now to add pagination

--- inv_manage/static/js/table.js
+++ inv_manage/static/js/table.js
@@ -3,8 +3,10 @@ let current_items = items.slice(0,10);
 
 let quantWidth = window.matchMedia("(max-width: 760px)");
 
-//TODO: Add pagination and the ability to sort descending
-let sort_columns = document.getElementsByClassName("sort-column");
+//TODO: Add pagination
+let sort_columns = document.getElementsByClassName("sort-column"); // columns to sort by
+let curr_column = ''; // column currently sorting by
+let ascend = true; // Sort ascending
 
 table = $(function(){
     $("#data-table").colResizable({
@@ -31,26 +33,53 @@ function replaceQuant(){
     }
 }
 
+// returns a function that sorts by the column you clicked on
 // I fucking hate javascript
 function sortClick(column){
     return function(){
+        col_name = [$(column).attr('sort-name')].toString();
+
         function compare(a, b){
-            console.log(a[$(column).attr('sort-name')])
-            if (a[$(column).attr('sort-name')] > b[$(column).attr('sort-name')]){
-                return 1;
+            a = a[col_name];
+            b = b[col_name];
+            if(typeof(a) != 'string'){
+                if (a > b){
+                    return 1;
+                }
+                if (a < b){
+                    return -1;
+                }
             }
-            if (a[$(column).attr('sort-name')] < b[$(column).attr('sort-name')]){
-                return -1;
+            else{
+                if (a.toLowerCase() > b.toLowerCase()){
+                    return 1;
+                }
+                if (a.toLowerCase() < b.toLowerCase()){
+                    return -1;
+                }
             }
             return 0;
         };
         items.sort(compare);
+        if(curr_column == col_name){
+            if(ascend == true){
+                ascend = false;
+                items = items.reverse()
+            }
+            else{
+                ascend = true;
+            }
+        }
+        else{
+            curr_column = col_name;
+            ascend = true;
+        }
+
         vue.items = items.slice(0,10);
     }
 }
 
 for(column of sort_columns){
-    console.log(column);
     column.addEventListener('click', sortClick(column))
 };
 
